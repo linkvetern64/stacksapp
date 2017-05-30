f07427b3608d6b160d279b950215c19399f9cd70
linkvetern64 <jstand1@umbc.edu>
2017-05-30 12:43:16 -0400

    JS AJAX implementation of inventory functions

--- stacksapp/view/js/inv-manager.js
+++ stacksapp/view/js/inv-manager.js
@@ -11,13 +11,13 @@ function populateInventory(){
     );
 }
 
-function populateSuccess(ajax){
+function populateSuccess(ajax) {
 
     var floors = ["floorOne", "floorTwo", "floorThree", "floorFour", "floorFive", "floorSix", "floorSeven"];
     var stacks = JSON.parse(ajax.responseText);
 
     /* Deletes all entries from the current table groupings */
-    for(var i = 0; i < floors.length; i++){
+    for (var i = 0; i < floors.length; i++) {
         //Gets container to populate entry listings
         var container = document.getElementById(floors[i]);
 
@@ -27,6 +27,7 @@ function populateSuccess(ajax){
         }
     }
 
+    /* Appends the listings to each floor grouping */
     for (var j = 0; j < stacks.length; j++) {
         var floor = stacks[j]["floor"] - 1;
         container = document.getElementById(floors[floor]);
@@ -37,11 +38,31 @@ function populateSuccess(ajax){
         entry.innerHTML = tag;
 
         container.appendChild(entry);
+    }
 
+    /* Adds append button to each section */
+    for (i = 0; i < floors.length; i++) {
+        container = document.getElementById(floors[i]);
+        entry = document.createElement('div');
+        entry.id = floors[i] + "Add";
+        entry.dataFloor = (i + 1);
+        entry.onclick = function(){updateListings(this)};
+        entry.className = "ghostLabel";
+        entry.innerHTML = "Add New " + "<span class='glyphicon glyphicon-plus s-gr'></span>";
+        container.appendChild(entry);
     }
 }
 
 function populateFailure(ajax){
     console.log("ERROR");
     console.log(ajax.responseText);
+}
+
+/**
+ * updateListings
+ * @param value - Passed in HTML Element
+ * @description Will be used for adding new elements to the inventory management table
+ */
+function updateListings(value){
+    console.log("Floor : " + value.dataFloor + " ID = " + value.id + " Parent : " + value.parentNode.id)
 }
